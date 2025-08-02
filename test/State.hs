1f134111c436fc785054b8c4099aa62afb3b6f6f
carp-sushi <105142707+carp-sushi@users.noreply.github.com>
2025-08-02 12:44:50 -0600

    use mvar in test state

--- test/State.hs
+++ test/State.hs
@@ -7,16 +7,17 @@ module State (
 import Domain
 import Service
 
-import Control.Concurrent.STM
+import Control.Concurrent.MVar
 import Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 
--- | In-memory STM storage type for testing.
-type State = TVar (HashMap Key Count)
+-- | In-memory storage type for testing.
+type State = MVar (HashMap Key Count)
 
 -- | Create an empty state.
 newState :: IO State
-newState = newTVarIO HM.empty
+newState =
+    newMVar HM.empty
 
 -- | Create a new counter test service.
 newCounterService :: State -> CounterService IO
@@ -29,11 +30,11 @@ newCounterService state =
 -- | Increment a counter under a key.
 stateCounterIncrement :: State -> Key -> Count -> IO ()
 stateCounterIncrement state key value =
-    atomically $
-        modifyTVar' state (HM.insertWith (+) key value)
+    modifyMVar_ state $
+        return . HM.insertWith (+) key value
 
 -- | Query a counter by key.
 stateCounterQuery :: State -> Key -> IO Count
 stateCounterQuery state key = do
-    counters <- readTVarIO state
+    counters <- readMVar state
     return $ HM.findWithDefault 0 key counters
